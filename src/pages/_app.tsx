44b8046983e41526c9f8427c6328af01323a48b5
OshadhaShiro <shirokumara49@gmail.com>
2023-01-26 23:07:23 +0530

    Display Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { HeaderBar } from '@/Components/Header';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<HeaderBar
+				links={[
+					{ link: '/', label: 'Home' },
+					{ link: '/About', label: 'About' },
+					{ link: '/Blog', label: 'Blog' },
+					{ link: '/Contact', label: 'Contact' },
+				]}
+			/>
+			<Component {...pageProps} />
+		</>
+	);
 }
