6be3fb6c3f3c5eff75e1538b73e6de37e9848ede
OshadhaShiro <shirokumara49@gmail.com>
2023-01-28 22:19:21 +0530

    Complete Header

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,142 @@
+import { useState } from 'react';
+import {
+	createStyles,
+	Header,
+	Container,
+	Group,
+	Burger,
+	Paper,
+	Transition,
+	Text,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+const HEADER_HEIGHT = 60;
+
+const useStyles = createStyles((theme) => ({
+	root: {
+		position: 'relative',
+		zIndex: 1,
+		backgroundColor: '#171717',
+		borderBlockWidth: '0',
+	},
+
+	dropdown: {
+		position: 'absolute',
+		top: HEADER_HEIGHT,
+		left: 0,
+		right: 0,
+		zIndex: 0,
+		borderTopRightRadius: 0,
+		borderTopLeftRadius: 0,
+		borderTopWidth: 0,
+		overflow: 'hidden',
+
+		[theme.fn.largerThan('sm')]: {
+			display: 'none',
+		},
+	},
+
+	header: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		height: '100%',
+	},
+
+	links: {
+		[theme.fn.smallerThan('sm')]: {
+			display: 'none',
+		},
+	},
+
+	burger: {
+		[theme.fn.largerThan('sm')]: {
+			display: 'none',
+		},
+	},
+
+	link: {
+		display: 'block',
+		lineHeight: 1,
+		padding: '8px 12px',
+		borderRadius: theme.radius.sm,
+		textDecoration: 'none',
+		color: theme.colors.dark[0],
+		fontSize: '12px',
+		fontWeight: 500,
+
+		'&:hover': {
+			backgroundColor: 'rgba(23, 90, 145, 0.3)',
+		},
+
+		[theme.fn.smallerThan('sm')]: {
+			color: '#111',
+			borderRadius: 0,
+			padding: theme.spacing.md,
+		},
+	},
+
+	linkActive: {
+		'&, &:hover': {
+			backgroundColor: 'rgba(23, 90, 145, 0.5)',
+			color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
+				.color,
+		},
+	},
+}));
+
+interface HeaderResponsiveProps {
+	links: { link: string; label: string }[];
+}
+
+export function HeaderBar({ links }: HeaderResponsiveProps) {
+	const [opened, { toggle, close }] = useDisclosure(false);
+	const [active, setActive] = useState(links[0].link);
+	const { classes, cx } = useStyles();
+
+	const items = links.map((link) => (
+		<a
+			key={link.label}
+			href={link.link}
+			className={cx(classes.link, {
+				[classes.linkActive]: active === link.link,
+			})}
+			onClick={(event) => {
+				event.preventDefault();
+				setActive(link.link);
+				close();
+			}}
+		>
+			{link.label}
+		</a>
+	));
+
+	return (
+		<Header height={HEADER_HEIGHT} mb={35} className={classes.root}>
+			<Container className={classes.header}>
+				<Text sx={{ color: '#fff', fontSize: '28px', fontWeight: 700 }}>
+					DevTalk
+				</Text>
+				<Group spacing={5} className={classes.links}>
+					{items}
+				</Group>
+
+				<Burger
+					opened={opened}
+					onClick={toggle}
+					className={classes.burger}
+					size="sm"
+				/>
+
+				<Transition transition="pop-top-right" duration={200} mounted={opened}>
+					{(styles) => (
+						<Paper className={classes.dropdown} withBorder style={styles}>
+							{items}
+						</Paper>
+					)}
+				</Transition>
+			</Container>
+		</Header>
+	);
+}
