44a472abf486316744eb1f41a3cdf12a823d1d01
OshadhaShiro <shirokumara49@gmail.com>
2023-01-28 15:18:32 +0530

    Create Dashboard Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,33 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
+import { Grid } from '@mantine/core';
 
 export default function Home() {
 	return (
-		<>
-			<h1>Home</h1>
-		</>
+		<Grid>
+			<Grid.Col
+				span={'auto'}
+				sx={{
+					backgroundColor: '#171717',
+					minHeight: 600,
+					borderRadius: '10px',
+				}}
+				m="lg"
+			>
+				Posts
+			</Grid.Col>
+			<Grid.Col
+				span={2}
+				sx={{
+					backgroundColor: '#171717',
+					minHeight: 600,
+					borderRadius: '10px',
+				}}
+				m="lg"
+			>
+				Menu
+			</Grid.Col>
+		</Grid>
 	);
 }
