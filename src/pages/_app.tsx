182a585e04f266eac740ec070e168195c210fd24
OshadhaShiro <shirokumara49@gmail.com>
2023-01-28 22:16:35 +0530

    Display Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { HeaderBar } from '@/Components/Header';
+import { HeaderBar } from '@/Components/Header/Header';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
