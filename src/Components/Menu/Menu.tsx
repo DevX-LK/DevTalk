711b31c609f0a9a10a6118f56a6921626fe78204
OshadhaShiro <shirokumara49@gmail.com>
2023-01-29 23:11:35 +0530

    Add icons to the Menu

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,12 +1,20 @@
-import { Button } from '@mantine/core';
+import { Button, Text } from '@mantine/core';
+import {
+	FcHome,
+	FcAbout,
+	FcBusinessman,
+	FcCollaboration,
+	FcBookmark,
+	FcCustomerSupport,
+} from 'react-icons/fc';
 
 const MenuItems = [
-	{ label: 'Home', link: '/' },
-	{ label: 'About', link: '/About' },
-	{ label: 'User Profile', link: '/me' },
-	{ label: 'Group', link: '/Groups' },
-	{ label: 'Blog', link: '/Blog' },
-	{ label: 'Group', link: '/groups' },
+	{ label: 'Home', link: '/', icon: <FcHome /> },
+	{ label: 'About', link: '/About', icon: <FcAbout /> },
+	{ label: 'User Profile', link: '/me', icon: <FcBusinessman /> },
+	{ label: 'Group', link: '/Groups', icon: <FcCollaboration /> },
+	{ label: 'Blog', link: '/Blog', icon: <FcBookmark /> },
+	{ label: 'Contact-us', link: '/Contact', icon: <FcCustomerSupport /> },
 ];
 
 const Menu = () => {
@@ -18,12 +26,25 @@ const Menu = () => {
 						background: 'transparent',
 						margin: '5px 0',
 						fontSize: '13px',
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'center',
 						'&:hover': {
 							background: 'rgba(23, 90, 145, 0.5)',
 						},
 					}}
 				>
 					{link.label}
+					<Text
+						sx={{
+							fontSize: '20px',
+							display: 'flex',
+							alignItems: 'center',
+							marginLeft: '4px',
+						}}
+					>
+						{link.icon}
+					</Text>
 				</Button>
 			))}
 		</>
