7bce94f69a781a10fb7f8f6fb6ea8f4e390c5c26
OshadhaShiro <shirokumara49@gmail.com>
2023-02-02 12:01:53 +0530

    Change color scheme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,39 @@
+import { useState } from 'react';
 import { HeaderBar } from '@/Components/Header/Header';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import {
+	ColorSchemeProvider,
+	ColorScheme,
+	MantineProvider,
+} from '@mantine/core';
 
 export default function App({ Component, pageProps }: AppProps) {
+	const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+
+	const toggleColorScheme = (value?: ColorScheme) =>
+		setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
 	return (
-		<>
-			<HeaderBar
-				links={[
-					{ link: '/', label: 'Home' },
-					{ link: '/About', label: 'About' },
-					{ link: '/Blog', label: 'Blog' },
-					{ link: '/Contact', label: 'Contact' },
-				]}
-			/>
-			<Component {...pageProps} />
-		</>
+		<ColorSchemeProvider
+			colorScheme={colorScheme}
+			toggleColorScheme={toggleColorScheme}
+		>
+			<MantineProvider
+				theme={{ colorScheme }}
+				withGlobalStyles
+				withNormalizeCSS
+			>
+				<HeaderBar
+					links={[
+						{ link: '/', label: 'Home' },
+						{ link: '/About', label: 'About' },
+						{ link: '/Blog', label: 'Blog' },
+						{ link: '/Contact', label: 'Contact' },
+					]}
+				/>
+				<Component {...pageProps} />
+			</MantineProvider>
+		</ColorSchemeProvider>
 	);
 }
