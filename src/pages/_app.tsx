38e6fa89de96726ad559e6f1e3a5252b93550337
OshadhaShiro <shirokumara49@gmail.com>
2023-02-14 23:48:58 +0530

    Check for logged in or out

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { HeaderBar } from '@/Components/Header/Header';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
@@ -12,9 +13,16 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
 	const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+	const router = useRouter();
 
 	const toggleColorScheme = (value?: ColorScheme) =>
 		setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			!localStorage.getItem('user') && router.push('/Register');
+		}
+	}, []);
 
 	return (
 		<ColorSchemeProvider
