735c6b00d50e949e125e2473308dc8378904011b
OshadhaShiro <shirokumara49@gmail.com>
2023-02-14 23:51:37 +0530

    Create CreatePosts page

---
+++ src/pages/CreatePosts.tsx
@@ -0,0 +1,13 @@
+import { Grid } from '@mantine/core';
+
+const CreatePost = () => {
+	return (
+		<Grid>
+			<Grid.Col>
+				<h1>CreatePost</h1>
+			</Grid.Col>
+		</Grid>
+	);
+};
+
+export default CreatePost;
