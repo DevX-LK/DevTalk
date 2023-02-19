f24232637811993f51132f4ad4cea67c9006b1c6
OshadhaShiro <shirokumara49@gmail.com>
2023-02-19 21:37:51 +0530

    Add inputs

--- src/pages/CreatePosts.tsx
+++ src/pages/CreatePosts.tsx
@@ -1,10 +1,25 @@
-import { Grid } from '@mantine/core';
+import { Button, Grid, Input, Textarea } from '@mantine/core';
 
 const CreatePost = () => {
 	return (
-		<Grid>
-			<Grid.Col>
-				<h1>CreatePost</h1>
+		<Grid sx={{ padding: '2rem', fontFamily: 'poppins' }}>
+			<Grid.Col span={'auto'}>
+				<Input
+					variant="filled"
+					placeholder="New Post Title"
+					size="xl"
+					sx={{ marginBottom: '2rem', fontWeight: 600 }}
+				/>
+				<Textarea
+					placeholder="Start..."
+					variant="filled"
+					radius="md"
+					size="xl"
+					sx={{ height: '700px' }}
+				/>
+			</Grid.Col>
+			<Grid.Col span={2}>
+				<Button sx={{ width: '100%' }}>Publish</Button>
 			</Grid.Col>
 		</Grid>
 	);
