99ad291810919dac94df82766b58019638abb46d
OshadhaShiro <shirokumara49@gmail.com>
2023-02-19 21:36:46 +0530

    Add route to CreatePost button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { FooterLinks } from '@/Components/Footer/Footer';
 import Menu from '@/Components/Menu/Menu';
 import Posts from '@/Components/Posts/Posts';
 import { Button, Grid } from '@mantine/core';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
@@ -34,16 +35,18 @@ export default function Home() {
 					},
 				}}
 			>
-				<Button
-					variant="outline"
-					sx={{
-						width: '100%',
-						height: '',
-						'&:hover': { background: 'rgb(23, 90, 170)', color: '#fff' },
-					}}
-				>
-					Create Post
-				</Button>
+				<Link href="/CreatePosts">
+					<Button
+						variant="outline"
+						sx={{
+							width: '100%',
+							height: '',
+							'&:hover': { background: 'rgb(23, 90, 170)', color: '#fff' },
+						}}
+					>
+						Create Post
+					</Button>
+				</Link>
 				<Grid.Col
 					span={'auto'}
 					sx={{
