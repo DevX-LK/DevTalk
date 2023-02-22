172c826fae6ecb762dc9727e4dc9064fb22afff2
OshadhaShiro <shirokumara49@gmail.com>
2023-02-22 22:45:59 +0530

    Add Inputs

--- src/pages/CreatePosts.tsx
+++ src/pages/CreatePosts.tsx
@@ -1,25 +1,60 @@
-import { Button, Grid, Input, Textarea } from '@mantine/core';
+import { Button, Grid, Input, createStyles } from '@mantine/core';
+import { useForm } from 'react-hook-form';
+
+const useStyles = createStyles({
+	contentInput: {
+		width: '100%',
+		marginBottom: '2rem',
+		fontWeight: 600,
+		fontSize: '25px',
+		outline: 'none',
+		border: '0',
+		padding: '1rem',
+		borderRadius: '6px',
+		fontFamily: 'poppins',
+		backgroundColor: '#252525',
+	},
+	contentTextbox: {
+		width: '100%',
+		height: '600px',
+		resize: 'none',
+		padding: '1rem',
+		outline: 'none',
+		border: '0',
+		borderRadius: '6px',
+		fontFamily: 'poppins',
+		backgroundColor: '#252525',
+	},
+});
 
 const CreatePost = () => {
+	const { classes, cx } = useStyles();
+
 	return (
-		<Grid sx={{ padding: '2rem', fontFamily: 'poppins' }}>
+		<Grid
+			sx={{
+				padding: '2rem',
+				fontFamily: 'poppins',
+				'@media (max-width: 728px)': { flexDirection: 'column' },
+			}}
+		>
 			<Grid.Col span={'auto'}>
-				<Input
-					variant="filled"
-					placeholder="New Post Title"
-					size="xl"
-					sx={{ marginBottom: '2rem', fontWeight: 600 }}
-				/>
-				<Textarea
-					placeholder="Start..."
-					variant="filled"
-					radius="md"
-					size="xl"
-					sx={{ height: '700px' }}
+				<input placeholder="New Post Title" className={classes.contentInput} />
+				<textarea
+					placeholder="New Post Content..."
+					className={classes.contentTextbox}
 				/>
 			</Grid.Col>
 			<Grid.Col span={2}>
-				<Button sx={{ width: '100%' }}>Publish</Button>
+				<Button
+					sx={{
+						width: '100%',
+						'@media (max-width: 728px)': { width: '100px' },
+					}}
+				>
+					Publish
+				</Button>
+				{/* Text Editor Pallete */}
 			</Grid.Col>
 		</Grid>
 	);
